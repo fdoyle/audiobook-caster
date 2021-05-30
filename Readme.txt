192a8723cc37bd3b4b7d234c431e2c377a8a6eee
frank doyle <frankd@franks-MacBook-Air.local>
2021-05-29 21:42:44 -0500

    create root directory

---
+++ Readme.txt
@@ -0,0 +1,15 @@
+Goal:
+
+Host folders arbitrary mp3's as podcasts for consumption in standard podcast apps.
+
+
+
+
+to-do:
+1: create a folder (if it does not exist) to store files
+2: manually put files in that folder in the appropriate format
+3: host those files as static content in the web-server
+4: have the app detect folders within the created root directory
+5: detect mp3 files within each folder
+6: create an RSS feed for each folder
+7: list all created RSS feeds on a home page
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,13 +3,28 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import java.io.File
+import java.nio.file.FileSystems
+import java.nio.file.Path
+
 
 fun main() {
+    val home = System.getProperty("user.home")
+    val rootPath = "$home/AudiobookCaster"
+    val rootPathFile = File(rootPath)
+    if(!rootPathFile.exists())
+        rootPathFile.mkdirs()
     embeddedServer(Netty, port = 8000) {
         routing {
             get("/") {
-                call.respondText("Hello, world!")
+                call.respondText("exists?")
             }
         }
     }.start(wait = true)
+}
+
+fun generateHomePage() : String{
+    return """
+        Hello World
+    """.trimIndent()
 }
